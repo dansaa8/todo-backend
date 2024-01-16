4b5f0434d6fd08fcc4beba61440eb84cc5d39254
Dan Saa <dansaa8@protonmail.com>
2024-01-16 08:46:04 +0100

    Create schema file + Task entity

--- src/main/java/com/example/todobackend/task/Task.java
+++ src/main/java/com/example/todobackend/task/Task.java
@@ -1,7 +1,6 @@
 package com.example.todobackend.task;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,30 +10,27 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @Setter
-@Table(name = "task", schema = "todo_app")
+@Table(name = "task", schema = "todo")
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Integer id;
 
-    @Size(max = 20)
-    @NotNull
+    @Size(max = 255)
     @Column(name = "name", nullable = false)
     private String name;
 
-    @NotNull
-    @Column(name = "user_id")
+    @Size(max = 255)
+    @Column(name = "user_id", nullable = false)
     private String userId;
 
-    @NotNull
     @Column(name = "isCompleted", nullable = false)
     private boolean isCompleted = false;
 
-    @NotNull
     @Column(name = "deadline", nullable = false)
     private LocalDateTime deadline;
 
-    @Column(name = "completed_at", nullable = false)
+    @Column(name = "completed_at")
     private LocalDateTime completedAt;
 }
