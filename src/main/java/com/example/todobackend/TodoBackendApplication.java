e9b8123b632bd708f300b63a7ca5db65f27e24fc
Dan Saa <dansaa8@protonmail.com>
2024-01-22 13:55:43 +0100

    Remove superfluous bean, manually configuring endpoints instead.

--- src/main/java/com/example/todobackend/TodoBackendApplication.java
+++ src/main/java/com/example/todobackend/TodoBackendApplication.java
@@ -3,8 +3,6 @@ package com.example.todobackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestClient;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -12,13 +10,6 @@ public class TodoBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoBackendApplication.class, args);
-	}
-
-	@Bean
-	public RestClient createClient() {
-		return RestClient.builder()
-				.baseUrl("https://localhost:8080")
-				.build();
 	}
 
 }
