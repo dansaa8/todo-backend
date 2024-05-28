6cc8449c469815233e2782823b9dcb2bdae0bbce
dansaa8 <dansaa8@protonmail.com>
2024-05-28 13:44:11 +0200

    TaskDTO now also returns description

--- src/main/java/com/example/todobackend/dto/TaskDTO.java
+++ src/main/java/com/example/todobackend/dto/TaskDTO.java
@@ -7,12 +7,14 @@ import java.time.LocalDateTime;
 public record TaskDTO(
         Long id,
         String name,
+        String description,
         LocalDateTime deadline,
         LocalDateTime completedAt) {
     public TaskDTO(Task task) {
         this(
                 task.getId(),
                 task.getName(),
+                task.getDescription(),
                 task.getDeadline(),
                 task.getCompletedAt()
         );
