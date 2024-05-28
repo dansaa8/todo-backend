c6ba3c9b85f4a81eefac6f2876a7370e887b7235
dansaa8 <dansaa8@protonmail.com>
2024-05-28 13:43:56 +0200

    Add description as an optional key in requestbody

--- src/main/java/com/example/todobackend/request/TaskCreateBody.java
+++ src/main/java/com/example/todobackend/request/TaskCreateBody.java
@@ -2,11 +2,13 @@ package com.example.todobackend.request;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
 
 import java.time.LocalDateTime;
 
 public record TaskCreateBody(
         @NotNull @NotEmpty String name,
+        String description,
         @NotNull LocalDateTime deadline
 ) {
 }
